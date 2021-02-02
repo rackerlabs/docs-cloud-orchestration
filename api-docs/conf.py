1d6336cc4fe4d45fe5b2e17b78724e7ce91ffe84
Ravi <raviteja.valluri@rackspace.com>
2021-02-02 13:48:08 -0800

    New redirects

--- api-docs/conf.py
+++ api-docs/conf.py
@@ -63,9 +63,6 @@ source_suffix = '.rst'
 # The master toctree document.
 master_doc = 'index'
 
-# The builder to use when running via the deconst preparer
-builder = 'deconst-serial'
-# builder = 'deconst-single'
 
 # General information about the project.
 project = 'Rackspace Developer Documentation'
